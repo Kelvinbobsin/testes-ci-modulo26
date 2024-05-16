0b484525d3a9509ccb3152cb3267accd424de29a
Kelvin Bobsin <kelvin.bobsin.1999@gmail.com>
2024-05-16 20:26:49 -0300

    jenkins

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,17 @@
-
-
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "mochawesome-report",
+      overwrite: false,
+      html: true,
+      json: true
+    }
   },
 });
--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,10 +1,6 @@
 /// <reference types="cypress" />
-import produtos from "../support/page_objects/produtos"
-import dadosprodutos from "../fixtures/produtos.json"
-
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-
     /*  Como cliente 
         Quero acessar a Loja EBAC 
         Para fazer um pedido de 4 produtos 
@@ -12,46 +8,12 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         Adicionando ao carrinho
         Preenchendo todas opções no checkout
         E validando minha compra ao final */
-        
 
-    beforeEach(() => {
-        cy.visit('minha-conta')
-        cy.fixture('perfil').then(dados => {
-            cy.login(dados.usuario, dados.senha)
-
-        })
-        cy.visit('produtos')
-
-    })
-
-
+   
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        cy.addprodutos('Abominable Hoodie', 'S', 'Blue', '4')
-        cy.get('.woocommerce-message').should('contain', '“Abominable Hoodie”')
+        //TODO 
     });
 
 
-
-    it('Deve preencher o checkout com sucesso', () => {
-        produtos.preencherCheckout(
-            dadosprodutos[1].nome,
-            dadosprodutos[1].sobrenome, 
-            dadosprodutos[1].empresa, 
-            dadosprodutos[1].pais, 
-            dadosprodutos[1].endereco, 
-            dadosprodutos[1].complemento, 
-            dadosprodutos[1].cidade, 
-            dadosprodutos[1].estado, 
-            dadosprodutos[1].cep, 
-            dadosprodutos[1].telefone, 
-            dadosprodutos[1].email
-
-        )
-
-        cy.get('.woocommerce-notice').should('contain' , 'Obrigado. Seu pedido foi recebido.')
-
-        
-    });
-
-});
+})
